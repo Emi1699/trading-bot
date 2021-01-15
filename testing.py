d30f587ba58efbfd6621a3d443a7c1941f1967b3
Emi1699 <emisv_16@yahoo.ro>
2021-01-15 21:49:12 +0200

    added .gitignore + getting familiar with API functionality

--- testing.py
+++ testing.py
@@ -1,30 +1,53 @@
 import requests
 import json
 import numpy as np
+
+# import sys
+# sys.path.insert(1, '/oanda-api-v20')
+
+from oandapyV20 import API
+import oandapyV20.endpoints.trades as trades
 # import pandas as pd
 
 #read access tokens from external file
-filepath = '../tokens.txt'
+filepath = '/Users/emi_buliga/Desktop/tokens.txt'
 tokens = []
 with open(filepath, 'r') as f:
 	for line in f:
 		tokens.append(line.strip())
 
-API = "api-fxpractice.oanda.com"
+apiToken = tokens[0]
+accToken = tokens[1]
+
+REST_API = "api-fxpractice.oanda.com"
 STREAM_API = "stream-fxpractice.oanda.com/"
 
-ACCESS_TOKEN = tokens[0]
-ACCOUNT_ID = tokens[1]
+ACCESS_TOKEN = apiToken
+ACCOUNT_ID = accToken
 
 PRICING_PATH = f"/v3/accounts/{ACCOUNT_ID}/pricing"
 
-query = {"instruments": "EUR_USD"} 
+query = {"instruments": "Gold"} 
 headers = {"Authorization": "Bearer "+ ACCESS_TOKEN}
 
-response = requests.get("https://"+API+PRICING_PATH, headers=headers, params=query)
+response = requests.get("https://"+REST_API+PRICING_PATH, headers=headers, params=query)
 
 data = response.json()
 
-print(tokens)
-print(data['prices'][0])
+# print(data)
+
+# for el in data['prices'][0]:
+# 	print(str(el) + " -> " + str(data['prices'][0][el]) + "\n")
+
+# get a list of trades
+
+#get trade history
+api = API(access_token = apiToken)
+accountID = accToken
+
+r = trades.TradesList(accountID)
+# show the endpoint as it is constructed for this call
+print("REQUEST:{}".format(r))
+rv = api.request(r)
+print("RESPONSE:\n{}".format(json.dumps(rv, indent=2)))
 
