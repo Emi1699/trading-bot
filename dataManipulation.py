ebe0533db1de35f1901e106afab327a4abaf357b
Emi1699 <emisv_16@yahoo.ro>
2021-01-16 23:58:26 +0200

    created new file that deals with reformating data from OANDA servers in a more readable way

---
+++ dataManipulation.py
@@ -0,0 +1,17 @@
+
+#write calendar date as DD/MM/YYY AND
+#only get the seconds, minutes and hour component of the time (without miliseconds)
+def newTime(oldTime):
+	#calendar date computation
+	calendarDateOld = oldTime[:10]
+
+	day = calendarDateOld[-2:]
+	month = calendarDateOld[5:7]
+	year = calendarDateOld[:4]
+
+	calendarDateNew = day + "." + month + "." + year
+
+	#clock time computation
+	clock = oldTime[11:19]
+
+	return (calendarDateNew, clock)
--- livePriceData.py
+++ livePriceData.py
@@ -4,6 +4,7 @@ import oandapyV20.endpoints.pricing as pricing
 import toks
 import json
 import time
+import dataManipulation as dm
 
 api = API(access_token=toks.apiToken)
 params = {"instruments": "EUR_USD"}
@@ -11,7 +12,13 @@ params = {"instruments": "EUR_USD"}
 r = pricing.PricingInfo(accountID=toks.accToken, params=params)
 rv = api.request(r)
 
-print(str(r.response['prices'][0]['instrument'])
+instrumentData = r.response['prices'][0]
+time = instrumentData['time']
+newTime = dm.newTime(time)
+
+print(str(instrumentData['instrument']) + " @ " + newTime[1] + " - " + newTime[0])
+
+askPrices = str(instrumentData['asks'])
 
 # while True:
 # 	print(r.response)
