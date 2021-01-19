fddf331cc511073e7100f1df0214ce3e9cbe8f06
Emi1699 <emisv_16@yahoo.ro>
2021-01-19 17:47:23 +0200

    now able to place basic trades on the market

--- trade.py
+++ trade.py
@@ -1,5 +1,38 @@
-
+import oandapyV20.endpoints.orders as orders
+import json
+from oandapyV20 import API
+import toks
 
 class Trade():
-	def __init__(self):
+	def __init__(self, apiToken = toks.AccessTokens().apiToken(), accToken = toks.AccessTokens().accToken()):
 		print("\n Trade Object created.\n")
+
+		self.apiToken = apiToken
+		self.accToken = accToken
+		self.api = API(access_token = self.apiToken)
+
+	def marketOrder(self, units, instrument):
+		orderConf = {
+	         "order": {
+	            "units": str(units),
+	            "instrument": instrument,
+	            "timeInForce": "IOC",
+	            "type": "MARKET",
+	            "positionFill": "DEFAULT"
+	          }
+	        }
+
+		r = orders.OrderCreate(accountID = self.accToken, data = orderConf)
+		print("processing : {}".format(r))
+		print("===============================")
+		print(r.data)
+		try:
+			response = self.api.request(r)
+		except V20Error as e:
+			print("V20Error: {}".format(e))
+		else:
+			print("Response: {}\n{}".format(r.status_code, json.dumps(response, indent=2)))
+
+trade = Trade()
+
+trade.marketOrder(1000, "EUR_USD")
