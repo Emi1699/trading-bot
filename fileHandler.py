09fa4c27ba5867c9dc2debe19f201cba86c16965
Emi1699 <emisv_16@yahoo.ro>
2021-01-19 19:54:11 +0200

    modified method names

--- fileHandler.py
+++ fileHandler.py
@@ -4,12 +4,22 @@ class FileHandler:
 	def __init__(self):
 		print("\nfileHandler Object created.\n")
 
-	def newEntryLiveData(self, file, content):
+	#creates a new entry in the specified file from the 'trades' directory
+	def newEntry_liveData(self, file, content):
 		f = open("price data/" + file, 'a+')
 		f.write(content + "\n")
 		f.close()
 
-	def newEntryTradePlaced(self, file, content):
+	#creates a new entry in the specified file from the 'trades' directory
+	#the line containts the id, number of units and price of the specified trade
+	def newEntry_tradePlaced(self, file, content):
 		f = open("trades/" + file, 'a+')
 		f.write(content + "\n")
 		f.close()
+
+	#when closing a trade, we also want to delete it from the 'trades' directory
+	def removeEntry_Trade(self, tradeID, file):
+		with open('trades/' + file) as oldfile, open('trades/' + file, 'w') as newfile:
+		    for line in oldfile:
+		        if not tradeID in line:
+		            newfile.write(line)
