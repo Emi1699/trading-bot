73c33f9204e7a9c5400e0ed7b4b93d5bbb57e15a
Emi1699 <emisv_16@yahoo.ro>
2021-01-19 18:48:42 +0200

    now saves successfully opened trades in a file

--- trade.py
+++ trade.py
@@ -2,6 +2,7 @@ import oandapyV20.endpoints.orders as orders
 import json
 from oandapyV20 import API
 import toks
+import fileHandler as fh
 
 class Trade():
 	def __init__(self, apiToken = toks.AccessTokens().apiToken(), accToken = toks.AccessTokens().accToken()):
@@ -10,6 +11,7 @@ class Trade():
 		self.apiToken = apiToken
 		self.accToken = accToken
 		self.api = API(access_token = self.apiToken)
+		self.fileHandler = fh.FileHandler()
 
 	def marketOrder(self, units, instrument):
 		orderConf = {
@@ -28,6 +30,14 @@ class Trade():
 		print(r.data)
 		try:
 			response = self.api.request(r)
+			
+			transactionID = str(response['orderFillTransaction']['tradeOpened']['tradeID'])
+			price = str(response['orderFillTransaction']['tradeOpened']['price'])
+			units = str(response['orderFillTransaction']['tradeOpened']['units'])
+			instrument = str(response['orderFillTransaction']['instrument'])
+
+			self.fileHandler.newEntryTradePlaced(instrument, "#" + transactionID + ' -> ' + units + " units @ " + price + "   ~~")
+
 		except V20Error as e:
 			print("V20Error: {}".format(e))
 		else:
