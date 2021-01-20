07137e65ddbf924abdc648124da381e2b1376ec0
Emi1699 <emisv_16@yahoo.ro>
2021-01-20 14:12:49 +0200

    user can place/close trades

--- user.py
+++ user.py
@@ -1,10 +1,12 @@
 import trade
+import liveDataStream
 
 class User:
 	def __init__(self):
 		print("\nUser instantiated.\n")
 
 		self.Trade = trade.Trade()
+		self.Stream = liveDataStream.LiveDataStream()
 
 	def placeTrade(self, units, instrument):
 		self.Trade.placeTrade(units, instrument)
@@ -12,4 +14,3 @@ class User:
 	def closeTrade(self, tradeID):
 		self.Trade.closeTrade(tradeID)
 
-	
