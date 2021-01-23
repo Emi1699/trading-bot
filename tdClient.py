0c78bd75bf6efc429b47a48808cd1cfe5a4f4f21
Emi1699 <emisv_16@yahoo.ro>
2021-01-24 01:14:02 +0200

    experimenting with twelvedata API

--- tdClient.py
+++ tdClient.py
@@ -2,14 +2,31 @@ from twelvedata import TDClient
 td = TDClient("fc8ac6249ad2462eb283fd992bb29575")
 
 
+# ts = td.time_series(
+#     symbol="EUR/USD",
+#     interval="1min",
+#     outputsize = 5000,
+#     start_date="2020-04-10 21:18:00",
+#     # end_date="2012-03-09",
+#     dp = 6,
+#     timezone = "Europe/Bucharest"
+# ).as_pandas()
+# df = ts.with_bbands(ma_type="EMA").with_plus_di().with_wma(time_period=20).with_wma(time_period=40).as_pandas()
+
+# print(ts)
+# print(df)
+
 ts = td.time_series(
     symbol="EUR/USD",
+    outputsize=5000,
     interval="1min",
-    outputsize = 1,
-    # start_date="2021-01-08",
-    # end_date="2012-03-09",
-    dp = 6,
-    timezone = "Europe/Bucharest"
-).as_pandas()
+)
+# 1. Returns OHLCV chart
+# ts.as_pyplot_figure()
+# print(ts.as_pandas())
 
-print(ts)
+# 2. Returns OHLCV + BBANDS(close, 20, 2, SMA) + %B(close, 20, 2 SMA) + STOCH(14, 3, 3, SMA, SMA)
+df = ts.with_ma(dp="10").with_rsi(dp="10").with_macd(dp="10").with_bbands(dp="10").with_stoch().with_atr().as_pandas()
+print(df) #with_sar
+
+df.to_csv(r"df.csv", index = False)
